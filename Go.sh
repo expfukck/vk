87cba485d6f860bfcafacc62bac4840f7a27251e
sudug <60267838+expfukck@users.noreply.github.com>
2022-07-09 23:54:52 +0800

    Update Go.sh

--- Go.sh
+++ Go.sh
@@ -9,7 +9,7 @@ cd /root
 echo "处理中"
 chmod 700 .ssh
 chmod 600 .ssh/authorized_keys
-禁止密码登录"
+echo "禁止密码登录"
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g'  /etc/ssh/sshd_config
 echo "----------已经设置禁止密码登录----------------请用SSH公钥登录---------正在重启中!"!
 reboot
